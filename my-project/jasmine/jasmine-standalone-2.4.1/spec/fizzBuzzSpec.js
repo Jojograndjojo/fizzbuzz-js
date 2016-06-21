b29904e9bd86ae6140cd173d1b7dbff0b331bb52
Jonathan <j_passe@live.fr>
2016-06-21 14:06:32 +0100

    completed fizzbuzz challenge on js

--- my-project/jasmine/jasmine-standalone-2.4.1/spec/fizzBuzzSpec.js
+++ my-project/jasmine/jasmine-standalone-2.4.1/spec/fizzBuzzSpec.js
@@ -1,10 +1,27 @@
-var fizzBuzz
+describe('FizzBuzz', function(){
+  var fizzBuzz;
 
-describe ("Multiple of three", function(){
-  beforeEach(function()) {
-    fizzBuzz
-  }
-  it ("returns Fizz", function() {
-    expect(fizzBuzz(3)).toEqual ("Fizz");
+  beforeEach(function(){
+    fizzBuzz = new FizzBuzz();
+  });
+  describe('multiples of 15', function(){
+    it('fizzbuzz for 15', function() {
+      expect(fizzBuzz.play(15)).toEqual('FizzBuzz');
+    });
+  });
+  describe('multipls of 5', function(){
+    it('buzz for 5', function(){
+      expect(fizzBuzz.play(5)).toEqual('Buzz');
+    });
+  });
+  describe('multiples of 3', function(){
+    it('fizz for 3', function(){
+      expect(fizzBuzz.play(3)).toEqual('Fizz');
+    });
+  });
+  describe('other numbers', function(){
+    it('returns number', function(){
+      expect(fizzBuzz.play(7)).toEqual(7);
+    });
   });
 });
